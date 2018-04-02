42c1b302bc2c53da44e78a09740e7af6fa8d178c
daisuke0131 <dai.tachikoma@gmail.com>
2018-04-02 09:16:25 +0900

    modify coincheck api

--- lib/cryptoexchange/exchanges/coincheck/services/market.rb
+++ lib/cryptoexchange/exchanges/coincheck/services/market.rb
@@ -8,27 +8,29 @@ module Cryptoexchange::Exchanges
           end
         end
 
-        def fetch(_)
-          output = super(ticker_url(_))
-          adapt(output)
+        def fetch(market_pair)
+          output = super(ticker_url(market_pair))
+          adapt(output,market_pair)
         end
 
-        def ticker_url(_)
-          "#{Cryptoexchange::Exchanges::Coincheck::Market::API_URL}/ticker"
+        def ticker_url(market_pair)
+          "#{Cryptoexchange::Exchanges::Coincheck::Market::API_URL}/api/rate/#{market_pair.base}_#{market_pair.target}"
         end
 
-        def adapt(output)
+        def adapt(output,market_pair)
           ticker           = Cryptoexchange::Models::Ticker.new
-          ticker.base      = 'BTC'
-          ticker.target    = 'JPY'
+          ticker.base      = market_pair.base
+          ticker.target    = market_pair.target
           ticker.market    = Coincheck::Market::NAME
-          ticker.ask       = NumericHelper.to_d(output['ask'])
-          ticker.bid       = NumericHelper.to_d(output['bid'])
-          ticker.last      = NumericHelper.to_d(output['last'])
-          ticker.high      = NumericHelper.to_d(output['high'])
-          ticker.low       = NumericHelper.to_d(output['low'])
-          ticker.volume    = NumericHelper.to_d(output['volume'])
-          ticker.timestamp = output['timestamp'].to_i
+          # ticker.ask       = NumericHelper.to_d(output['ask'])
+          # ticker.bid       = NumericHelper.to_d(output['bid'])
+          # ticker.last      = NumericHelper.to_d(output['last'])
+          # ticker.high      = NumericHelper.to_d(output['high'])
+          # ticker.low       = NumericHelper.to_d(output['low'])
+          # ticker.volume    = NumericHelper.to_d(output['volume'])
+          # ticker.timestamp = output['timestamp'].to_i
+          ticker.last      = NumericHelper.to_d(output['rate'])
+          ticker.timestamp = Time.now.to_i
           ticker.payload   = output
           ticker
         end
